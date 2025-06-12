727528fa586b035e78e693962b336a5b6ba8d3de
scaevola34 <gamer2734@gmail.com>
2025-06-12 18:32:33 +0200

    Update ArtistDashboard.tsx

--- src/pages/ArtistDashboard.tsx
+++ src/pages/ArtistDashboard.tsx
@@ -1,7 +1,6 @@
 
 import React from 'react';
 import { ArtistDashboard as ArtistDashboardComponent } from '@/components/ArtistDashboard';
-import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
 
 const ArtistDashboard = () => {
